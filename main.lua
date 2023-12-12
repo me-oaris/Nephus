a08451c540c057cce5495cf841d608939c18dea7
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2023-12-12 16:51:36 +0545

    Update main.lua

--- main.lua
+++ main.lua
@@ -45,13 +45,11 @@ local Toggle = MainTab:CreateToggle({
    CurrentValue = false,
    Flag = "aguess", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-            if Value = false then
             game.StarterGui:SetCore("SendNotification", {
             Title = "Nephus Hub",
             Text = "This function is disabled",
             Duration = 5
                     })
-            end
    -- The function that takes place when the toggle is pressed
    -- The variable (Value) is a boolean on whether the toggle is true or false
    end,
