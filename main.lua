e46df544521a2fa5a7d43395375ca34a28748e54
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2023-12-12 17:44:39 +0545

    Update main.lua

--- main.lua
+++ main.lua
@@ -40,22 +40,3 @@ local MainSection = MainTab:CreateSection("Status")
 local Label = MainTab:CreateLabel("Playing Song : NaN")
 
 local SecondSection = MainTab:CreateSection("Farm")
-local Toggle = MainTab:CreateToggle({
-   Name = "Auto Guess",
-   CurrentValue = false,
-   Flag = "aguess", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-            if Value == false then
-            game.StarterGui:SetCore("SendNotification", {
-            Title = "Nephus Hub",
-            Text = "This function is disabled",
-            Duration = 5
-                    })
-            end
-   -- The function that takes place when the toggle is pressed
-   -- The variable (Value) is a boolean on whether the toggle is true or false
-   end,
-})
-
-local ThirdSection = MainTab:CreateSection("Misc")
-
