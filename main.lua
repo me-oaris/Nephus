af6b55515f4391378c6bb74fce29ea9356188ca7
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2023-12-15 16:26:19 +0545

    Update main.lua
    Changed to fluent

--- main.lua
+++ main.lua
@@ -1,42 +1,42 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "Nephus Hub",
-   LoadingTitle = "Starting Nephus Hub!",
-   LoadingSubtitle = "by Sakchem",
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Nephus Hub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Nephus Keysystem",
-      Subtitle = "Enter an key to use Nephus!",
-      Note = "Key can be obtained from the discord server.",
-      FileName = "nephuskey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"areyougay"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local Window = Fluent:CreateWindow({
+    Title = "Nephus Hub" .. Fluent.Version,
+    SubTitle = "by Sakchem",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
-Rayfield:Notify({
-   Title = "Nephus Notification",
-   Content = "Thank You For Using Nephus!",
-   Duration = 6.5,
-   Image = nil,
-   Actions = nil
-})
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "home" }),
+    Settings = Window:AddTab({ Title = "Misc", Icon = "diamond" })
+}
 
-local MainTab = Window:CreateTab("Main", nil) -- Title, Image
+local Options = Fluent.Options
 
-local MainSection = MainTab:CreateSection("Status")
-local Label = MainTab:CreateLabel("Playing Song : NaN")
+do
+    Fluent:Notify({
+        Title = "Nephus Hub",
+        Content = "Thankyou for using Nephus!",
+        SubContent = "By Sakchem", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
 
-local SecondSection = MainTab:CreateSection("Farm")
+    Tabs.Main:AddParagraph({
+        Title = "Currently Playing:",
+        Content = " NaN"
+    })
+
+    local Toggle = Tabs.Main:AddToggle("auto_guess", {Title = "Auto Guess", Default = false })
+
+    Toggle:OnChanged(function()
+        print("Auto Guess Turned:", Options.MyToggle.Value)
+    end)
+
+    Options.auto_guess:SetValue(false)
+end
