bdbef9b8f0127ffe6d71db177d961f7059cdecd8
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2023-12-15 19:08:29 +0545

    Update main.lua

--- main.lua
+++ main.lua
@@ -97,7 +97,7 @@ local SpeedSlider = Tabs.Misc:AddSlider("Slider", {
     Max = 100,
     Rounding = 1,
     Callback = function(Value)
-        game.Workspace.oaris_script.Humanoid.WalkSpeed(Value)
+        game.Workspace.LocalPlayer.Humanoid:SetAttribute("WalkSpeed",Value)
     end
 })
 
@@ -115,7 +115,7 @@ local SizeSlider = Tabs.Misc:AddSlider("Slider", {
     Max = 100,
     Rounding = 1,
     Callback = function(Value)
-        game.Workspace.oaris_script.Humanoid.WalkSpeed(Value)
+        game.Workspace.LocalPlayer.Humanoid:SetAttribute("WalkSpeed",Value)
     end
 })
 
