bb46c52cb69a89227b79020ba1d9ab061c232a02
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2023-12-15 17:40:00 +0545

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,72 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-local http = require("socket.http")
-local json = require("json")
+
+-- Assuming http and json are properly loaded
+
+local function sendhook(Action)
+    local player = game.Players.LocalPlayer
+
+    if player then
+        -- Fetch user information from Roblox API
+        local success, userData = pcall(function()
+            return game.Players:GetUserThumbnailAsync(player.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size100x100), player.DisplayName
+        end)
+
+        if success then
+            -- Discord webhook URL
+            local webhookUrl = "https://discord.com/api/webhooks/1185179760452702309/ZrCPGRXnsQkAA6RCB2NXHZXZfVOvgm7opEwQts_wARfCmKAai2z1eQPI2TdZWBlscLbd"
+
+            -- Create payload with content, embed, and thumbnail
+            local payload = {
+                content = Action,
+                embeds = {
+                    {
+                        author = {
+                            name = userData,
+                        },
+                        thumbnail = {
+                            url = userData,
+                        },
+                        description = Action,
+                        fields = {
+                            {
+                                name = "Additional Field",
+                                value = "You can add more fields here.",
+                            },
+                        },
+                    },
+                },
+            }
+
+            -- Send payload to Discord webhook
+            local success, response = pcall(function()
+                return game.HttpService:PostAsync(webhookUrl, game.HttpService:JSONEncode(payload), Enum.HttpContentType.ApplicationJson, false)
+            end)
+
+            if success and response then
+                print("Webhook message sent successfully")
+            else
+                print("Failed to send webhook message. Response:", response)
+            end
+        else
+            print("Failed to fetch Roblox user data.")
+        end
+    else
+        print("Local player not found.")
+    end
+end
+
+-- Assuming Fluent and other libraries are loaded
 
 local Window = Fluent:CreateWindow({
     Title = "Nephus Hub V1",
     SubTitle = "by Sakchem",
     TabWidth = 100,
     Size = UDim2.fromOffset(580, 460),
-    Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
+    Acrylic = false,
     Theme = "Darker",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+    MinimizeKey = Enum.KeyCode.LeftControl
 })
 
 local Tabs = {
@@ -21,85 +76,23 @@ local Tabs = {
 
 local Options = Fluent.Options
 
-do
-    Fluent:Notify({
-        Title = "Nephus Hub",
-        Content = "Thankyou for using Nephus!",
-        SubContent = "By Sakchem", -- Optional
-        Duration = 5 -- Set to nil to make the notification not disappear
-    })
+Tabs.Main:AddParagraph({
+    Title = "Currently Playing:",
+    Content = " NaN"
+})
 
-    Tabs.Main:AddParagraph({
-        Title = "Currently Playing:",
-        Content = " NaN"
-    })
+local Toggle = Tabs.Main:AddToggle("auto_guess", {Title = "Auto Guess", Default = false })
 
-    local Toggle = Tabs.Main:AddToggle("auto_guess", {Title = "Auto Guess", Default = false })
+Toggle:OnChanged(function()
+    print("Auto Guess Turned:", Options.auto_guess.Value)
+end)
 
-    Toggle:OnChanged(function()
-        print("Auto Guess Turned:", Options.auto_guess.Value)
-    end)
+Options.auto_guess:SetValue(false)
 
-    Options.auto_guess:SetValue(false)
-
-    Tabs.Misc:AddButton({
-        Title = "Button of nothingness",
-        Description = "This button does nothing",
-        Callback = function()
-            local playerId = 12345678 -- Replace with the actual Roblox user ID
-    
-            -- Fetch user information from Roblox API
-            local robloxApiUrl = "https://api.roblox.com/users/" .. playerId
-            local response, statusCode, _, _ = http.request(robloxApiUrl)
-    
-            if statusCode == 200 then
-                local userData = json.decode(response)
-    
-                -- Discord webhook URL
-                local webhookUrl = "https://discord.com/api/webhooks/1185179760452702309/ZrCPGRXnsQkAA6RCB2NXHZXZfVOvgm7opEwQts_wARfCmKAai2z1eQPI2TdZWBlscLbd"
-    
-                -- Create payload with content, embed, and thumbnail
-                local payload = {
-                    content = "Button of nothingness was pressed!",
-                    embeds = {
-                        {
-                            author = {
-                                name = userData.Username,
-                            },
-                            thumbnail = {
-                                url = "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&username=" .. userData.Username,
-                            },
-                            fields = {
-                                {
-                                    name = "Additional Field",
-                                    value = "You can add more fields here.",
-                                },
-                            },
-                        },
-                    },
-                }
-    
-                -- Send payload to Discord webhook
-                local _, statusCode = http.request {
-                    url = webhookUrl,
-                    method = "POST",
-                    headers = {
-                        ["Content-Type"] = "application/json",
-                    },
-                    source = json.encode(payload),
-                }
-    
-                if statusCode == 200 then
-                    print("Webhook message sent successfully")
-                else
-                    print("Failed to send webhook message. Status code:", statusCode)
-                end
-            else
-                print("Failed to fetch Roblox user data. Status code:", statusCode)
-            end
-        end
-    })
-
-
-    
-end
+Tabs.Misc:AddButton({
+    Title = "Button of nothingness",
+    Description = "This button does nothing",
+    Callback = function()
+        sendhook("Button of nothingness was pressed!")
+    end
+})
