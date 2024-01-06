b3ee73a294e72b8e3089da780fadd89180ade693
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2024-01-06 11:24:01 +0545

    Create wrpd.lua

---
+++ wrpd.lua
@@ -0,0 +1,44 @@
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+
+-- Functions
+_G.autolevel = false
+
+-- Declatarions
+local songButton = workspace.Settings.selectedsong
+
+-- UI elements
+local win = Flux:Window("Nephus Hub", "By Sakchem", Color3.fromRGB(62, 180, 137), Enum.KeyCode.LeftControl)
+local Main = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+Main:Toggle("Autofarm Levels", "*Warn* Only Use In Private", function(t)
+_G.autolevel = t
+end)
+
+--Scripts 
+while _G.lvlfarm == true do
+	local args = {
+    [1] = songButton:GetAttribute("Title")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PlayAnimation"):FireServer(unpack(args))
+Wait(0.1)
+end
+
+-- Toggle script button
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+
+local customAssetID = "15619828392"
+
+local button = Instance.new("ImageButton")
+button.Size = UDim2.new(0, 50, 0, 50) 
+button.Position = UDim2.new(0.5, -300, 0.5, -100) 
+button.AnchorPoint = Vector2.new(0.5, 0.5)
+button.Image = "rbxassetid://" .. customAssetID
+button.Parent = gui
+
+
+button.MouseButton1Click:Connect(function()
+game:GetService('VirtualInputManager'):SendKeyEvent(true, 'LeftControl', false, yes)
+end)
+    
