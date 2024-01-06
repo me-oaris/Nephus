ec2ae1c7bccb1bc073a1b7481fb494cdd201ecf0
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2024-01-06 22:41:02 +0545

    Update wrpd.lua

--- wrpd.lua
+++ wrpd.lua
@@ -4,6 +4,19 @@ local Luxt = Luxtl.CreateWindow("Nephus Hub", 15894229415)
 -- Declarations
 local songButton = workspace.Settings.selectedsong
 local answergui = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.AnswerGui
+local gui = Instance.new("ScreenGui")
+gui.ResetOnSpawn = false
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+
+local customAssetID = "15894229415"
+local button = Instance.new("ImageButton")
+button.Size = UDim2.new(0, 50, 0, 50) 
+button.Position = UDim2.new(0.5, -300, 0.5, -100) 
+button.AnchorPoint = Vector2.new(0.5, 0.5)
+button.Image = "rbxassetid://" .. customAssetID
+button.Parent = gui
+button.BackgroundColor3 = Color3.new(0, 0, 0) 
+
 
 -- Ui elements
 
@@ -45,4 +58,8 @@ auto:Toggle("Auto Levels *RISKY*", function(a)
         game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PlayAnimation"):FireServer(unpack(args))
         wait(0.1)
     end
+end)
+
+button.MouseButton1Click:Connect(function()
+    game:GetService('VirtualInputManager'):SendKeyEvent(true,'LeftAlt', false, yes)
 end)
