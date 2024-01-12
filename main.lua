3e91f92bc1c52ff41d0c89380d887eabb6890d2f
나는 오아리스다 <109019792+me-oaris@users.noreply.github.com>
2024-01-12 09:18:06 +0545

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 8548412569 then 
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/me-oaris/Nephus/main/wrpd.lua"))()
 else
 local user = game:GetService("Players").LocalPlayer
 user:kick("Game Not Supported")
